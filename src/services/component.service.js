c0a76809df24e5d6b71c4e09b8d35a1d1d4d8114
Paul Lockyer <plockyer@googlemail.com>
2024-11-11 12:36:09 +0000

    method to return all components

--- src/services/component.service.js
+++ src/services/component.service.js
@@ -2,6 +2,44 @@
 const db = require('../database/db');
 const validator = require('../utils/validation');
 
+/**
+ * Get a list of components
+ */
+const findComponents = async () => {
+
+    try{
+
+        /* Create the SQL statement and any supporting values required */
+        const sqlStmt = "SELECT id, name, owner, description FROM components";
+
+        /* Excute the SQL stement against the DB and store the results found */
+        const results = await db.query(sqlStmt);
+
+        if(!results || results?.rows?.length <= 0){
+            return {
+                "state": "fail",
+                "message": "Unable to get list of components",
+                "data": []
+            }
+        } else {
+            return {
+                "state": "ok",
+                "message": "Components successfully found",
+                "data": results?.rows
+            }
+        }
+
+    } catch(error) {
+        console.log(error);
+        return {
+            "state": "fail",
+            "message": error.message,
+            "data": []
+        }
+    }
+
+};
+
 module.exports = {
-    
+    findComponents
 }
