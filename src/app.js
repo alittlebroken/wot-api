a5776b412f061519f2983b48ca6875e9ed99e244
Paul Lockyer <plockyer@googlemail.com>
2024-11-12 14:18:41 +0000

    added routing for components resource

--- src/app.js
+++ src/app.js
@@ -11,8 +11,10 @@ app.use(express.urlencoded({extended: true}));
 
 const measurementsRoute = require("./routes/api/v1/measurements.route");
 const devicesRoute = require('./routes/api/v1/devices.route');
+const componentsRoute = require('./routes/api/v1/components.route');
 
 app.use('/api/v1/measurements', measurementsRoute);
 app.use('/api/v1/devices', devicesRoute);
+app.use('/api/v1/components', componentsRoute);
 
 module.exports = app;
