296784ed3fbba025d4d09e3692ccfb7e6dd6e76d
Paul Lockyer <plockyer@googlemail.com>
2024-11-14 12:20:49 +0000

    initial module commit

---
+++ src/services/auth.service.js
@@ -0,0 +1,12 @@
+/* Import supporting libraries and files */
+const db = require('../database/db');
+const validator = require('../utils/validation');
+const config = require('../config/config');
+const bc = require('bcrypt');
+const userService = require('./user.service');
+
+
+
+module.exports = {
+
+}
