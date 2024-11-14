c06f6031064ae44455f807606e2c1bcf6d6a1594
Paul Lockyer <plockyer@googlemail.com>
2024-11-14 15:39:39 +0000

    added registration auth route

--- src/routes/api/v1/auth.routes.js
+++ src/routes/api/v1/auth.routes.js
@@ -3,5 +3,6 @@ const route = express.Router();
 const controller = require('../../../controllers/auth.controller');
 
 route.post('/login', controller.login);
+route.post('/register', controller.registerUser);
 
 module.exports = route;
