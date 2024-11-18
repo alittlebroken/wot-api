9d3017448679969c3f32cce34ce8f1df9365dc2c
Paul Lockyer <plockyer@googlemail.com>
2024-11-18 15:20:21 +0000

    aded the apiKeys route to the express server

--- src/app.js
+++ src/app.js
@@ -14,11 +14,13 @@ const devicesRoute = require('./routes/api/v1/devices.route');
 const componentsRoute = require('./routes/api/v1/components.route');
 const authRoutes = require('./routes/api/v1/auth.routes');
 const usersRoutes = require('./routes/api/v1/users.route');
+const apiKeysRoutes = require('./routes/api/v1/apikeys.routes');
 
 app.use('/api/v1/measurements', measurementsRoute);
 app.use('/api/v1/devices', devicesRoute);
 app.use('/api/v1/components', componentsRoute);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', usersRoutes);
+app.use('/api/v1/apikeys', apiKeysRoutes);
 
 module.exports = app;
