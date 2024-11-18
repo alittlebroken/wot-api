8498e3a6ec6e61cbc6d3abb17065ea36d990bd51
Paul Lockyer <plockyer@googlemail.com>
2024-11-18 17:48:06 +0000

    added check to ensure only valid devices can post measurements

--- src/routes/api/v1/measurements.route.js
+++ src/routes/api/v1/measurements.route.js
@@ -5,7 +5,7 @@ const auth = require('../../../middleware/auth.middleware');
 
 route.get('/', auth.isAuthenticated, controller.findMeasurments);
 route.get('/:id', auth.isAuthenticated, controller.findMeasurement);
-route.post('/', controller.createMeasurement);
+route.post('/', auth.isValidDevice, controller.createMeasurement);
 route.put('/:id', auth.isAuthenticated, controller.updateMeasurement);
 route.delete('/:id', auth.isAuthenticated, controller.removeMeasurement);
 
