98ee987e00f6d9ccf870495fc27f8b32e0f3abd4
Paul Lockyer <plockyer@googlemail.com>
2024-11-19 19:28:42 +0000

    added logout route

--- src/routes/api/v1/auth.routes.js
+++ src/routes/api/v1/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const route = express.Router();
 const controller = require('../../../controllers/auth.controller');
+const auth = require('../../../middleware/auth.middleware');
 
 route.post('/login', controller.login);
+route.post('/logout', auth.isAuthenticated, controller.logout);
 route.post('/register', controller.registerUser);
 
 module.exports = route;
