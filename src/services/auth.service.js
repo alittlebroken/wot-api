2dff5da43e69f1f99346b13248f74b04b659e79b
Paul Lockyer <plockyer@googlemail.com>
2024-11-19 17:19:45 +0000

    removed uneeded libs and removed JWT token creation

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,6 @@
 /* Import supporting libraries and files */
-const db = require('../database/db');
 const validator = require('../utils/validation');
-const config = require('../config/config');
 const bc = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const userService = require('./user.service');
 
 /**
@@ -41,23 +38,11 @@ const login = async (username, password) => {
                 }
             } else {
 
-                /* The user is valid and they have provided a valid password
-                 Now we can create the HWT tokens */
-                const data = user.data[0];
-                const payload = {
-                    id: data.id,
-                    username: data.username,
-                    display_name: data.display_name,
-                }
-
-                /* set the options for the token like Expiry */
-                const options = { expiresIn: config.JWT_DEFAULT_EXPIRY || '5m' }
-
-                const token = await jwt.sign(payload, config.JWT_SECRET_TOKEN, options);
-
-                /* Before sending the token back ensure that it is valid */
-                if(token && token.length > 0){
-                    return token;
+                /* we have successfully logged in */
+                return {
+                    "state": "ok",
+                    "message": "Logged in successfully",
+                    "data": []
                 }
 
             }
