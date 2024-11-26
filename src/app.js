a1c98b89f9cfcfde71e80926d21064ee41378c6f
Paul Lockyer <plockyer@googlemail.com>
2024-11-26 11:12:47 +0000

    updated allowed headers

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+/* Aloows the use of cookies */
+app.use(cookieParser());
+
 app.use(cors({
     origin: ['http://localhost:5173'],
     credentials: true,
-    allowedHeaders: "authorisation,Authorisation,Content-Type,content-type,Content-type,token,range",
+    allowedHeaders: "authorisation,Authorisation,Content-Type,content-type,Content-type,token,range, crossorigin",
     exposedHeaders: "Content-Range, X-Content-Range"
 }));
 
 /* Set the options for handling JSON via the body of the request */
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-/* Aloows the use of cookies */
-app.use(cookieParser());
 
 const measurementsRoute = require("./routes/api/v1/measurements.route");
 const devicesRoute = require('./routes/api/v1/devices.route');
